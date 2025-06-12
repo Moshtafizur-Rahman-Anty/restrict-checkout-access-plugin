f2d3912001e3572ef8fc3e3fbeb47feb078d05eb
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2025-06-12 13:16:43 +0600

    fixed HPOS support text

--- restrict-checkout-access.php
+++ restrict-checkout-access.php
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 
-// ✅ Declare HPOS compatibility properly
+// ✅ Declare HPOS compatibility
 add_action( 'before_woocommerce_init', function() {
     if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
         \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
